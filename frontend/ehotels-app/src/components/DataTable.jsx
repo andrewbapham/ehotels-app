c1d29d068d0c1680162c06b36c3513012698697d
Andrew Pham <andrewbapham@outlook.com>
2023-04-03 11:25:29 -0400

    Added table for viewing data from DB

---
+++ frontend/ehotels-app/src/components/DataTable.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+
+function DataTable(props) {
+  if (props.data.length === 0) return <div></div>;
+  const headers = [];
+  //Extract the table headers using the first row of data
+  Object.keys(props.data[0]).forEach((item) =>
+    headers.push(<th key={item}>{item}</th>)
+  );
+
+  const tableRows = [];
+  props.data.forEach((item) => {
+    const cells = [];
+    // For each key/header, create a cell with the corresponding value
+    Object.keys(props.data[0]).forEach((column) => {
+      cells.push(
+        //Create cell with column name + id as key
+        <td key={column + "_" + Object.values(item)[0]}>{item[column]}</td>
+      );
+    });
+
+    //Push all cells into one table row, with the id as the key
+    tableRows.push(<tr key={Object.values(item)[0]}>{cells}</tr>);
+  });
+  return (
+    <table>
+      <thead>
+        <tr>{headers}</tr>
+      </thead>
+      {tableRows}
+    </table>
+  );
+}
+
+export default DataTable;
--- frontend/ehotels-app/src/components/EmployeeView.jsx
+++ frontend/ehotels-app/src/components/EmployeeView.jsx
@@ -1,16 +1,39 @@
-import React from "react";
+import { useState, useEffect, React } from "react";
+import DataTable from "./DataTable";
+import axios from "axios";
+
+const baseURL = "http://localhost:3000";
 
 function EmployeeView(props) {
+  const [table, setTable] = useState("customer");
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get(baseURL + "/api/employee?table=" + table)
+      .then((res) => {
+        setData(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, [table]);
+
   return (
     <div>
       <h3>Employee</h3>
-      <label for="table-name">Data:</label>
-      <select name="table-select" id="table-select">
-        <option value="Customer">Customer</option>
-        <option value="Employees">Employees</option>
-        <option value="Hotel">Hotel</option>
-        <option value="Room">Room</option>
+      <label htmlFor="table-name">Data:</label>
+      <select
+        name="table-select"
+        id="table-select"
+        onChange={(e) => setTable(e.target.value)}
+      >
+        <option value="customer">Customer</option>
+        <option value="employees">Employees</option>
+        <option value="hotel">Hotel</option>
+        <option value="room">Room</option>
       </select>
+      {data.length === 0 ? "" : <DataTable data={data} />}
     </div>
   );
 }
