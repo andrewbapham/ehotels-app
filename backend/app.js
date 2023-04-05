4d00e56b200933486bf2e367c38d89e5dad00277
Andrew Pham <andrewbapham@outlook.com>
2023-04-05 17:49:37 -0400

    Added endpoint for retrieving table column names

--- backend/app.js
+++ backend/app.js
@@ -12,6 +12,7 @@ const db = mysql.createConnection({
   database: "ehotels_db",
 });
 
+//Enable CORS to allow requests from frontend on localhost:3000
 app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header(
@@ -30,18 +31,30 @@ db.connect((err) => {
 });
 
 app.get("/", (req, res) => {
-  //console.log(req);
   res.send("Hello World!");
 });
 
+//Returns all rows from the requested table
 app.get("/api/employee", (req, res) => {
-  //console.log(req);
   db.query("SELECT * FROM " + req.query.table, (err, result) => {
     if (err) {
       console.log(err);
     } else {
       res.send(result);
       //console.log(result);
+    }
+  });
+});
+
+//Returns column headers for the requested table
+app.get("/api/employee/headers", (req, res) => {
+  console.log("received");
+  db.query("SHOW COLUMNS FROM " + req.query.table, (err, result) => {
+    if (err) {
+      console.log(err);
+    } else {
+      const headers = result.map((col) => col.Field);
+      res.send(headers);
     }
   });
 });
