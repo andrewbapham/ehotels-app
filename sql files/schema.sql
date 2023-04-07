1d561c54114240e04a491ee38fa4c2cb2ef899ba
Andrew Pham <andrewbapham@outlook.com>
2023-04-07 17:43:36 -0400

    Add auto_increment for primary keys and set nulls

--- sql files/schema.sql
+++ sql files/schema.sql
@@ -1,7 +1,7 @@
 CREATE DATABASE ehotels_db;
 USE ehotels_db;
 CREATE TABLE Hotel_Chain (
-    Chain_id INT PRIMARY KEY,
+    Chain_id INT PRIMARY KEY AUTO_INCREMENT,
     Central_office_address VARCHAR(255) NOT NULL,
     Number_of_hotels INT NOT NULL,
     Email_addresses VARCHAR(255),
@@ -9,7 +9,7 @@ CREATE TABLE Hotel_Chain (
 );
 
 CREATE TABLE Hotel (
-    Hotel_id INT PRIMARY KEY,
+    Hotel_id INT PRIMARY KEY AUTO_INCREMENT,
     Stars INT NOT NULL CHECK (Stars > 0 AND Stars <= 5),
     Number_of_rooms INT NOT NULL CHECK (Number_of_rooms >= 0),
     Address VARCHAR(255) NOT NULL,
@@ -21,7 +21,7 @@ CREATE TABLE Hotel (
 );
 
 CREATE TABLE Room (
-    Room_id INT PRIMARY KEY,
+    Room_id INT PRIMARY KEY AUTO_INCREMENT,
     Price DECIMAL(10,2) NOT NULL CHECK (Price > 0),
     Amenities VARCHAR(255),
     Capacity INT NOT NULL CHECK (Capacity > 0),
@@ -33,7 +33,7 @@ CREATE TABLE Room (
 );
 
 CREATE TABLE Customer (
-    Customer_id INT PRIMARY KEY,
+    Customer_id INT PRIMARY KEY AUTO_INCREMENT,
     Customer_name VARCHAR(255) NOT NULL,
     Address VARCHAR(255) NOT NULL,
     SSN VARCHAR(255) UNIQUE NOT NULL,
@@ -41,7 +41,7 @@ CREATE TABLE Customer (
 );
 
 CREATE TABLE Employee (
-    Employee_id INT PRIMARY KEY,
+    Employee_id INT PRIMARY KEY AUTO_INCREMENT,
     Employee_Name VARCHAR(255) NOT NULL,
     Address VARCHAR(255),
     SSN VARCHAR(255) UNIQUE NOT NULL,
@@ -51,20 +51,20 @@ CREATE TABLE Employee (
 );
 
 CREATE TABLE Booking (
-    Booking_id INT PRIMARY KEY,
-    Customer_id INT NOT NULL,
+    Booking_id INT PRIMARY KEY AUTO_INCREMENT,
+    Customer_id INT,
     Start_date DATE NOT NULL,
     End_date DATE NOT NULL,
     Price DECIMAL(10,2),
-    Room_id INT NOT NULL,
-    FOREIGN KEY (Customer_id) REFERENCES Customer(Customer_id) ON DELETE SET NULL,
-    FOREIGN KEY (Room_id) REFERENCES Room(Room_id) ON DELETE SET NULL
+    Room_id INT,
+    FOREIGN KEY (Customer_id) REFERENCES Customer(Customer_id),
+    FOREIGN KEY (Room_id) REFERENCES Room(Room_id)
 );
 
 CREATE TABLE Renting (
-    Rent_id INT PRIMARY KEY,
+    Rent_id INT PRIMARY KEY AUTO_INCREMENT,
     Booking_id INT NOT NULL,
     Start_date DATE NOT NULL,
     End_date DATE NOT NULL,
-    FOREIGN KEY (Booking_id) REFERENCES Booking(Booking_id) ON DELETE SET NULL
+    FOREIGN KEY (Booking_id) REFERENCES Booking(Booking_id)
 );
