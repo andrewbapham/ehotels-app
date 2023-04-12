12d20bc49e19f481bfaa8f7e8010b108a227de08
Ash Bhattarai <abhat109@uottawa.ca>
2023-04-12 14:33:21 -0400

    Added views for number of available rooms per area and capacity of all rooms of a specific hotel

--- sql files/triggersAndViews.sql
+++ sql files/triggersAndViews.sql
@@ -62,3 +62,17 @@ END //
 DELIMITER ;
 CREATE VIEW occupiedRooms AS
 SELECT r.room_id, b.start_date, b.end_date FROM room r, booking b WHERE r.room_id = b.room_id;
+
+CREATE VIEW room_capacity_by_hotel AS
+SELECT h.hotel_name, SUM(r.capacity) AS total_capacity
+FROM hotel h
+JOIN room r ON h.hotel_id = r.hotel_id
+GROUP BY h.hotel_name;
+
+CREATE VIEW available_rooms_per_area AS
+SELECT h.city, COUNT(r.room_id) AS num_available_rooms
+FROM hotel h
+JOIN room r ON h.hotel_id = r.hotel_id
+LEFT JOIN booking b ON r.room_id = b.room_id AND (CURDATE() BETWEEN b.start_date AND b.end_date)
+WHERE b.booking_id IS NULL
+GROUP BY h.city;
