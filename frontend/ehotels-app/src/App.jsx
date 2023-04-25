d71d92359dcf13a5fd339d417ed40714ad1a63ae
Andrew Pham <andrewbapham@outlook.com>
2023-04-25 16:34:40 -0400

    Added routing for client and employee pages

--- frontend/ehotels-app/src/App.jsx
+++ frontend/ehotels-app/src/App.jsx
@@ -2,15 +2,20 @@ import { useState } from "react";
 import CustomerView from "./components/CustomerView";
 import EmployeeView from "./components/EmployeeView";
 import "./App.css";
+import { Link } from "react-router-dom";
 
 function App() {
-  const [role, setRole] = useState("customer");
-
   return (
     <div className="App">
       <div className="role-selector">
         <h3>Select a role:</h3>
-        <input
+        <Link to={"/customer"}>
+          <button>Customer</button>
+        </Link>
+        <Link to={"/employee"}>
+          <button>Employee</button>
+        </Link>
+        {/* <input
           type="radio"
           id="customer-role"
           name="user-role"
@@ -26,9 +31,8 @@ function App() {
           value="employee"
           onClick={() => setRole("employee")}
         />
-        <label htmlFor="employee-role">Employee</label>
+        <label htmlFor="employee-role">Employee</label> */}
       </div>
-      {role === "customer" ? <CustomerView /> : <EmployeeView />}
     </div>
   );
 }
--- frontend/ehotels-app/src/main.jsx
+++ frontend/ehotels-app/src/main.jsx
@@ -1,10 +1,30 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import ErrorPage from "./ErrorPage";
+import CustomerView from "./components/CustomerView";
+import EmployeeView from "./components/EmployeeView";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <ErrorPage />,
+  },
+  {
+    path: "/customer",
+    element: <CustomerView />,
+  },
+  {
+    path: "/employee",
+    element: <EmployeeView />,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
